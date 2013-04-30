fd32baecdf3ba162d938b040c09c54005a46abed
Andy Stanberry <andystanberry@gmail.com>
2013-04-30 11:45:26 -0400

    Refactor responses

--- jirafe/client.py
+++ jirafe/client.py
@@ -3,11 +3,22 @@ import requests
 
 class JirafeClient(object):
     version = 'v1'
-    def __init__(self, api_url='https://api.jirafe.com'):
+    url_mask = '{url}{version}/{site_id}/{path}'
+    def __init__(self, api_url='https://api.jirafe.com/', requests=requests):
         self.api_url = api_url if api_url.endswith('/') else api_url + '/'
+        self.requests = requests
 
     def product_change(self, session, data):
         return self._put(session, 'product', data)
+
+    def _get_url(self, session, path):
+        url_data = {
+            'url': self.api_url,
+            'version': self.version,
+            'site_id': session.site_id,
+            'path': path,
+        }
+        return self.url_mask.format(**url_data)
 
     def _put(self, session, path, data, retry=0):
         if type(data) is not str:
@@ -17,11 +28,31 @@ class JirafeClient(object):
             "data": data,
             "headers": session.get_header()
         }
+        response = self.requests.put(self._get_url(session, path), **options)
 
-        r = requests.put('%s%s/%s/%s' % (self.api_url, self.version, session.site_id, path), **options)
 
-        if r.status_code not in (200,503) and retry < 1:
-            session.invalidate()
-            return self._put(session, path, data, 1)
+        if response.status_code == 200:
+            return {
+                'success': True
+            }
+        elif response.status_code == 400:
+            data = response.json()
+            return {
+                'success': False,
+                'error_type': 'validation',
+                'errors': data['errors'] if 'errors' in data else {}
+            }
+        elif response.status_code == 403:
+            if retry < 1:
+                session.invalidate()
+                return self._put(session, path, data, 1)
+            else:
+                return {
+                    'success': False,
+                    'error_type': 'authorization'
+                }
         else:
-            return r.json()
+            return {
+                'success': False,
+                'error_type': 'unknown',
+            }
