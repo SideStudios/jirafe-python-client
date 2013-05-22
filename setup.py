dddbff76edbc584906a59b5e8d3087e32cd380e1
Andy Stanberry <andystanberry@gmail.com>
2013-05-22 17:12:08 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="jirafe-python-client",
-    version="0.4.1",
+    version="0.5",
     url="https://github.com/jirafe/jirafe-python-client",
     packages=["jirafe"],
     license="Copyright 2013, Jirafe under MIT License",
